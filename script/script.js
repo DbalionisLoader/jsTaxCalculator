f8a89675a43d321428da219ce03c352ec63bb28d
Dovydas B <27741806+DbalionisLoader@users.noreply.github.com>
2023-08-10 15:12:20 +0100

    Added decimal number validation

--- script/script.js
+++ script/script.js
@@ -1,10 +1,10 @@
 //console.log("hello js working");
 //To do
 /*
--Form heading
-- One value input
-- one button
-- one output field
+-Form heading DONE
+- One value input DONE
+- one button DONE
+- one output field DONE
 **WORKING***
 */
 /* To do 2
@@ -13,12 +13,13 @@
 -Calc total using the radio input
 -validate form 
     - change lebel text 
-    - change form borded to red
-    - add empty and int only validation
+    - change form borded to red DONE
+    - add empty and int only validation DONE
 */
 
 const form = document.getElementById('tipForm');
 const totalInput = document.getElementById('preTipTotalInput');
+const element = document.getElementById("billTotalHelp");
 
 // Functions 
 /* function validateForm() {
@@ -29,11 +30,16 @@ const totalInput = document.getElementById('preTipTotalInput');
     }
   } */
 
+function removeHelper() {
+  element.remove(); // Removes the div with the 'div-02' id
+}
+
+
 //Function add error message below input field and turn swap css class to make border red/green
 const setError = (element, errorMessage) => {
           const inputControl = element.parentElement;
           const errorDisplay = inputControl.querySelector('.error');
-         
+          removeHelper();
           errorDisplay.innerText = errorMessage;
           inputControl.classList.add('error');
           inputControl.classList.remove('success');
@@ -62,14 +68,24 @@ document.getElementById('tipBtn').addEventListener('click', e => {
 } );
 
 
+//Function from https://www.w3resource.com/javascript-exercises/javascript-math-exercise-38.php to check if number decimal or not. 
+//RETURN: TRUE if decimal.
+function isDecimal(value) {
+  return result = (value - Math.floor(value)) !== 0; 
+}
+
+//Validation for int only input field
 const validateInputs = () => {
   //Trim value to get rid of empty space
   const tipInput = totalInput.value.trim(); 
 
   if(tipInput === ''){
     setError(totalInput, "Your bill total required");
-  } else if ( isNaN(tipInput)) {
+  } else if ( isNaN(tipInput) ) {
      setError(totalInput, "Please enter only a number");
+  } else if ( isDecimal(tipInput)) {
+    console.log(isDecimal(tipInput));
+    setError(totalInput, "Please enter only WHOLE numbers");
   } else { 
       setSuccess(totalInput);
       //call calculate tip method only if all validation pass
@@ -82,14 +98,8 @@ const validateInputs = () => {
 
 function calcTip(){
     
-   /*  if (validateForm() == false){
-        console.log("No input");
-    } else {
-    
-    const tax = 0.2; */
-
     let preTipTotal = parseInt(document.getElementById("preTipTotalInput").value);
-   //Tip radio fetch
+   //Tip radio fetch - fetch array - select value from .checked 
    const radioButtons = document.querySelectorAll('input[name="tipAmount"]');
     let selectedTip;
        for (const radioButton of radioButtons) {
